29be0a23699c6b20d9b9de68c15118aa487073f1
KimKiHyun0206 <rlarlgus0206@naver.com>
2022-08-22 08:50:45 +0900

    후배 코드 리팩토링 후 피드백

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,45 @@
+import java.util.*;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        //inputFormula
+        String formula_text = scan.nextLine();
+
+        //formulaNumberArray -> formulaNumbers
+        String[] formula_numAry = formula_text
+                .replaceAll("\\s", "")
+                .split("\\+|-|\\*|/");
+
+        //formulaSymbolArray -> formulaSymbols
+        String[] formula_symbolAry = formula_text
+                .replaceAll("\\s", "")
+                .replaceAll("[0-9]", "")
+                .split("");
+
+
+        int result = 0;
+        for (int z = 0; z < formula_symbolAry.length; z++) {
+            if (z > 0) {
+                if (formula_symbolAry[z].equals("+")) result += Integer.parseInt(formula_numAry[z + 1]);
+
+                else if (formula_symbolAry[z].equals("-")) result -= Integer.parseInt(formula_numAry[z + 1]);
+
+                else if (formula_symbolAry[z].equals("*")) result *= Integer.parseInt(formula_numAry[z + 1]);
+
+                else if (formula_symbolAry[z].equals("/")) result /= Integer.parseInt(formula_numAry[z + 1]);
+            } else {
+                if (formula_symbolAry[z].equals("+")) result = Integer.parseInt(formula_numAry[z]) + Integer.parseInt(formula_numAry[z + 1]);
+
+                else if (formula_symbolAry[z].equals("-")) result = Integer.parseInt(formula_numAry[z]) - Integer.parseInt(formula_numAry[z + 1]);
+
+                else if (formula_symbolAry[z].equals("*")) result = Integer.parseInt(formula_numAry[z]) * Integer.parseInt(formula_numAry[z + 1]);
+
+                else if (formula_symbolAry[z].equals("/")) result = Integer.parseInt(formula_numAry[z]) / Integer.parseInt(formula_numAry[z + 1]);
+            }
+        }
+        System.out.println("answer = "+result);
+    }
 }
